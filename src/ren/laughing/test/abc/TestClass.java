799215963646d2acaa933a297d4f274b3313c5b0
Laughing_Lz <645596034@qq.com>
2016-08-24 21:57:12 +0800

    test
    RT

---
+++ src/ren/laughing/test/abc/TestClass.java
@@ -0,0 +1,14 @@
+package ren.laughing.test.abc;
+
+public class TestClass {
+	private static void testMethod() {
+		System.out.println("testMethod");
+	}
+
+	public static void main(String[] args) {
+		((TestClass) null).testMethod();
+	}
+	public void function() {
+		String str = "yes";
+	}
+}
